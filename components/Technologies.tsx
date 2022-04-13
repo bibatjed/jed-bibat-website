db2e0c4e30a6c8837897488326e2cf0fbfebc37a
Jeeed <43069218+bibatjed@users.noreply.github.com>
2022-04-13 20:31:50 +0800

    feat: added skills

---
+++ components/Technologies.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+interface Props {
+  name: string;
+  icon: React.ReactNode;
+}
+export default function Technologies(props: Props) {
+  return (
+    <div className="flex items-center space-x-4 p-3">
+      <span className="h-13 w-13 rounded-full p-2 shadow-md border-0 border-gray-600 bg-gray-800">
+        {props.icon}
+      </span>
+      <p className="text-gray-200 text-lg"> {props.name}</p>
+    </div>
+  );
+}
---
+++ constants/techonologies.js
@@ -0,0 +1,39 @@
+import ReactLogo from "../components/icons/React";
+import GolangLogo from "../components/icons/Go";
+import NodeLogo from "../components/icons/Node";
+import MySQLLogo from "../components/icons/Mysql";
+import Typescript from "../components/icons/Typescript";
+import Javascript from "../components/icons/Javascript";
+export default [
+  {
+    id: 1,
+    name: "React",
+    icon: <ReactLogo />,
+  },
+  {
+    id: 2,
+
+    name: "Node",
+    icon: <NodeLogo />,
+  },
+  {
+    id: 3,
+    name: "Go",
+    icon: <GolangLogo />,
+  },
+  {
+    id: 4,
+    name: "MySQL",
+    icon: <MySQLLogo />,
+  },
+  {
+    id: 5,
+    name: "Typescript",
+    icon: <Typescript />,
+  },
+  {
+    id: 6,
+    name: "Javascript",
+    icon: <Javascript />,
+  },
+];
